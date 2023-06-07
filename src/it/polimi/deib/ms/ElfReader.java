7f5e08548a608bb93273a5fa097f87b5afa08724
adile78 <adilembo78@gmail.com>
2023-06-07 11:50:17 +0200

    Commit 7/06

---
+++ src/it/polimi/deib/ms/ElfReader.java
@@ -0,0 +1,18 @@
+package it.polimi.deib.ms;
+
+public class ElfReader {
+
+    int[] elf = {
+            207,
+            250,
+            254,
+            7,
+            0,
+            0,
+            128,
+            2,
+            0,
+            0,
+            0
+    };
+}
